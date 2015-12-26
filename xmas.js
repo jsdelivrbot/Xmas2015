36529a7561a23f9f942c0d9d3f89e14ac2570098
AllNamedRTaken <joel.brage@gmail.com>
2015-12-26 20:31:59 +0100

    Performance and new Templating function

--- xmas.js
+++ xmas.js
@@ -1,9 +1,27 @@
+var TemplateType;
+(function (TemplateType) {
+    TemplateType[TemplateType["static"] = 0] = "static";
+    TemplateType[TemplateType["general"] = 1] = "general";
+    TemplateType[TemplateType["text"] = 2] = "text";
+    TemplateType[TemplateType["attr"] = 3] = "attr";
+})(TemplateType || (TemplateType = {}));
+function base64ToArrayBuffer(base64String) {
+    var byteNumbers = new Array(base64String.length);
+    for (var i = 0; i < base64String.length; i++) {
+        byteNumbers[i] = base64String.charCodeAt(i);
+    }
+    var byteArray = new Uint8Array(byteNumbers);
+    return byteArray.buffer;
+}
 var Xmas = (function () {
     function Xmas() {
-        this.css = "#thepanel {position:absolute; width:100%; height:100%; z-index:100000; top:0; left:0; background-color:#000; opacity:0.6;}\n\n\t.flake {z-index: 1; position:absolute; top:0; left:0; font-size:" + (Xmas.BaseFlakeHeight / 1.3) + "px; width:" + Xmas.BaseFlakeHeight + "px; height:" + Xmas.BaseFlakeHeight + "px; color: #ffffff; opacity: 1.0; text-align: center;}\n\n\t.star {z-index: 1; position:absolute; top:0; left:0; font-size:" + (Xmas.BaseStarHeight / 1.3) + "px; width:" + Xmas.BaseStarHeight + "px; height:" + Xmas.BaseStarHeight + "px; color: #ffffff; opacity: 0.6; text-align: center;}\n\n\t#thegreeting {font-family: \"Comic Sans MS\", cursive, sans-serif; z-index: 2; position:absolute; top:0; left:0; right:0; bottom:0; margin:auto; height:300px; width: 100%; font-size:180px; line-height:normal; background-color:transparent; color: #f00000; vertical-align:middle; text-align:center; white-space: nowrap;} \n\t@-webkit-keyframes twinkle { 0% { opacity: 0.6; } 90% { opacity: 0.6; } 95% { opacity: 1.0; }} \n\t@keyframes twinkle { 0% { opacity: 0.6; } 90% { opacity: 0.6; } 95% { opacity: 1.0; }}\n\t.star {-webkit-animation: twinkle 5s infinite; animation: twinkle 5s infinite;} \n\t";
-        this.FlakeTemplate = "<div id=\"{$id}\" class=\"flake\">{Xmas.getRandomFlake()}</div>";
-        this.StarTemplate = "<div id=\"{$id}\" class=\"star\">{Xmas.getRandomStar()}</div>";
-        this.GreetingTemplate = "<div id=\"thegreeting\">{this.getMessage()}</div>";
+        this.css = "#thepanel {transform:translateZ(0); position:absolute; overflow:hidden; width:100%; height:100%; z-index:100000; top:0; left:0; background-color:#000; opacity:0.6;}\n\n\t.flake {z-index: 1; position:absolute; top:0; left:0; font-size:" + (Xmas.BaseFlakeHeight / 1.3) + "px; width:" + Xmas.BaseFlakeHeight + "px; height:" + Xmas.BaseFlakeHeight + "px; color: #ffffff; opacity: 1.0; text-align: center;}\n\n\t.star {z-index: 1; position:absolute; top:0; left:0; font-size:" + (Xmas.BaseStarHeight / 1.3) + "px; width:" + Xmas.BaseStarHeight + "px; height:" + Xmas.BaseStarHeight + "px; color: #ffffff; opacity: 0.6; text-align: center;}\n\n\t#thegreeting {font-family: \"Comic Sans MS\", cursive, sans-serif; z-index: 2; position:absolute; top:0; left:0; right:0; bottom:0; margin:auto; height:300px; width: 100%; font-size:180px; line-height:normal; background-color:transparent; color: #f00000; vertical-align:middle; text-align:center; white-space: nowrap;} \n\t@-webkit-keyframes twinkle { 0% { opacity: 0.6; } 90% { opacity: 0.6; } 95% { opacity: 1.0; }} \n\t@keyframes twinkle { 0% { opacity: 0.6; } 90% { opacity: 0.6; } 95% { opacity: 1.0; }}\n\t.star {-webkit-animation: twinkle 5s infinite; animation: twinkle 5s infinite;} \n\t";
+        this.FlakeTemplateString = "<div id=\"{$id}\" class=\"flake\">{:text for $id:Xmas.getRandomFlake()}</div>";
+        this.StarTemplateString = "<div id=\"{$id}\" class=\"star\">{:text for $id:Xmas.getRandomStar()}</div>";
+        this.GreetingTemplateString = "<div id=\"thegreeting\">{:text for thegreeting:this.getMessage()}</div>";
+        this.FlakeTemplate = null;
+        this.StarTemplate = null;
+        this.GreetingTemplate = null;
         this.Greeting = null;
         this.Panel = null;
         this.Style = null;
@@ -25,6 +43,7 @@ var Xmas = (function () {
         this.greetTime = 0;
         this.greetPos = 0;
         this.timeToGreet = false;
+        this.mouse = { x: 0, y: 0 };
         this.Panel = Xmas.createElement("<div id=\"thepanel\"></div>");
         this.Style = Xmas.createElement(this.css, "style");
         setTimeout(this.start.bind(this));
@@ -60,52 +79,150 @@ var Xmas = (function () {
     Xmas.getRandomStar = function () {
         return Xmas.getRandomChar(Xmas.StarChars);
     };
-    Xmas.drawTemplate = function (template, parent, id, context) {
+    Xmas.compileTemplate = function (template) {
         var templateParts = [];
         var pos = 0;
-        var s = 0, e = 0;
+        var s = 0;
         var expr = "";
-        var obj = {};
+        var next = 1;
+        var templateType = TemplateType.static;
+        var forId = null;
         while (pos < template.length) {
-            s = template.indexOf("{", pos);
-            if (s === -1) {
-                templateParts.push(template.substring(pos));
-                break;
+            switch (template[pos]) {
+                case "\\":
+                    pos++;
+                    break;
+                case "{":
+                    templateParts.push({
+                        part: template.substring(s, pos),
+                        templateType: templateType,
+                        forId: forId
+                    });
+                    next = pos + 1;
+                    if (template.indexOf(":text", next) === next) {
+                        //optimize text content replace
+                        templateType = TemplateType.text;
+                        pos += 5;
+                        if (template.indexOf(" for ", pos) === pos + 1) {
+                            s = pos += 5;
+                            pos = template.indexOf(":", pos);
+                            if (pos === -1) {
+                                throw "Malfomed template";
+                            }
+                            forId = template.substring(s, pos).trim();
+                        }
+                        pos = template.indexOf(":", pos);
+                        if (pos === -1) {
+                            throw "Malfomed template";
+                        }
+                    }
+                    else if (template.indexOf(":attr", next) === next) {
+                        //optimize text content replace
+                        templateType = TemplateType.attr;
+                        pos += 5;
+                        if (template.indexOf(" for ", pos) === pos + 1) {
+                            s = pos += 5;
+                            pos = template.indexOf(":", pos);
+                            if (pos === -1) {
+                                throw "Malfomed template";
+                            }
+                            forId = template.substring(s, pos).trim();
+                        }
+                        else {
+                            pos = template.indexOf(":", pos);
+                            if (pos === -1) {
+                                throw "Malfomed template";
+                            }
+                        }
+                    }
+                    s = pos + 1;
+                    break;
+                case "}":
+                    if (pos > 1) {
+                        templateParts.push({
+                            part: template.substring(s, pos),
+                            templateType: templateType,
+                            forId: forId
+                        });
+                        s = pos + 1;
+                        templateType = TemplateType.static;
+                        forId = null;
+                    }
+                    break;
             }
-            templateParts.push(template.substring(pos, s));
-            s++;
-            e = template.indexOf("}", s);
-            if (e === -1) {
-                throw ("DUDE! your template sucks");
-            }
-            expr = "return " + template.substring(s, e);
-            if (id !== undefined && expr.indexOf("$id") !== -1) {
-                expr = expr.replace("$id", "\"id\"");
-            }
-            var fn = new Function(expr);
-            templateParts.push(fn.call(context || this));
-            pos = ++e;
+            pos++;
         }
-        template = templateParts.join("");
-        var el = Xmas.createElement(template);
-        var existing;
-        if (existing = parent.querySelector("#" + id)) {
-            parent.replaceChild(el, existing);
+        if (s !== pos) {
+            templateParts.push({
+                part: template.substring(s, pos),
+                templateType: templateType,
+                forId: forId
+            });
         }
-        else {
-            parent.appendChild(el);
+        return templateParts;
+    };
+    Xmas.drawTemplate = function (template, parent, id, context) {
+        var templateParts = [];
+        var p = 0;
+        var obj = {};
+        var html = "";
+        var el = null;
+        var existing = id ? (el = parent.querySelector("#" + id)) : null;
+        var expr = "";
+        var fn = null;
+        for (p = 0; p < template.length; p++) {
+            switch (template[p].templateType) {
+                case TemplateType.text:
+                    expr = template[p].part.replace("$id", id);
+                    fn = new Function("return " + expr);
+                    expr = fn.call(context || this);
+                    if (existing) {
+                        parent.querySelector("#" + template[p].forId).textContent = expr;
+                    }
+                    else {
+                        templateParts.push(expr);
+                    }
+                    break;
+                case TemplateType.attr:
+                    break;
+                case TemplateType.general:
+                    break;
+                default:
+                    //static
+                    if (!existing) {
+                        expr = template[p].part.replace("$id", id);
+                        templateParts.push(expr);
+                    }
+                    break;
+            }
+        }
+        if (!existing) {
+            html = templateParts.join("");
+            el = Xmas.createElement(html);
+            if (existing) {
+                parent.replaceChild(el, existing);
+            }
+            else {
+                parent.appendChild(el);
+            }
         }
         return el;
     };
     Xmas.prototype.getMessage = function () {
         return Xmas.Greetings[this.greetPos];
     };
+    Xmas.prototype.mousemove = function (event) {
+        this.mouse.x = event.clientX;
+        this.mouse.y = event.clientY;
+    };
     Xmas.prototype.start = function () {
-        var _this = this;
         var i;
         var windowWidth = Xmas.WindowWidth = window.innerWidth;
         var windowHeight = Xmas.WindowHeight = window.innerHeight;
         var starSpan = windowHeight / 3.0;
+        this.StarTemplate = Xmas.compileTemplate(this.StarTemplateString);
+        this.FlakeTemplate = Xmas.compileTemplate(this.FlakeTemplateString);
+        this.GreetingTemplate = Xmas.compileTemplate(this.GreetingTemplateString);
         this.Greeting = Xmas.drawTemplate(this.GreetingTemplate, this.Panel, "thegreeting", this);
         for (i = 0; i < this.starCount; i++) {
             this.stars.push(Xmas.drawTemplate(this.StarTemplate, this.Panel, "star" + i));
@@ -123,11 +240,17 @@ var Xmas = (function () {
             this.flakes[i].style.transform = "translate(" + this.flakePos[i].x + "px, " + this.flakePos[i].y + "px) scale(" + this.flakeSpeed[i] + ") rotateY(" + this.flakeRotation[i] + "deg)";
             this.flakes[i].style.zIndex = (this.flakeScale[i] < 1.0 ? 1 : 3).toString();
         }
-        this.Panel.addEventListener("click", function () { _this.destroy(); });
-        this.Panel.addEventListener("touchend", function () { _this.destroy(); });
-        this.Panel.addEventListener("pointerup", function () { _this.destroy(); });
+        this.Panel.addEventListener("click", this.destroy.bind(this));
+        this.Panel.addEventListener("touchend", this.destroy.bind(this));
+        this.Panel.addEventListener("pointerup", this.destroy.bind(this));
+        this.Panel.addEventListener("mousemove", this.mousemove.bind(this));
         document.body.appendChild(this.Style);
         document.body.appendChild(this.Panel);
+        /*
+        AudioManager.loadSound("holiday", base64Music, true).then((id:string) => {
+            AudioManager.playSound(id);
+        });
+        */
         this.msPerFrame = 1000.0 / Xmas.FPS;
         this.running = true;
         this.oldTime = window.performance.now();
@@ -141,14 +264,18 @@ var Xmas = (function () {
         this.time = window.performance.now();
         var i;
         var greetDiff = this.time - this.greetTime;
+        var flakeHit = false;
         if (greetDiff > Xmas.GreetTimeout) {
             this.timeToGreet = true;
             this.greetTime = this.time;
             this.greetPos = (this.greetPos + 1) % Xmas.Greetings.length;
         }
         for (i = 0; i < this.flakeCount; i++) {
-            if (this.flakePos[i].y > Xmas.WindowHeight) {
+            flakeHit = ((this.mouse.x >= this.flakePos[i].x) && (this.mouse.x - this.flakePos[i].x) < Xmas.BaseFlakeHeight * this.flakeScale[i]) &&
+                ((this.mouse.y >= this.flakePos[i].y) && (this.mouse.y - this.flakePos[i].y) < Xmas.BaseFlakeHeight * this.flakeScale[i]);
+            if (flakeHit || this.flakePos[i].y > Xmas.WindowHeight) {
                 this.flakePos[i].y -= (Xmas.WindowHeight + 100);
+                this.flakePos[i].x = Xmas.randomInt(Xmas.WindowWidth);
             }
             this.flakePos[i].y += Xmas.WindowHeight * this.flakeSpeed[i] / Xmas.FPS;
             this.flakeRotation[i] = (this.flakeRotation[i] + this.flakeRotationSpeed[i]) % 360;
@@ -159,6 +286,7 @@ var Xmas = (function () {
         var i;
         if (this.timeToGreet) {
             this.Greeting = Xmas.drawTemplate(this.GreetingTemplate, this.Panel, "thegreeting", this);
+            this.timeToGreet = false;
         }
         for (i = 0; i < this.flakeCount; i++) {
             this.flakes[i].style.transform = "translate(" + this.flakePos[i].x + "px, " + this.flakePos[i].y + "px) scale(" + this.flakeScale[i] + ") rotateY(" + this.flakeRotation[i] + "deg)";
--- xmas.ts
+++ xmas.ts
@@ -2,6 +2,31 @@ interface IVec2 {
 	x: number;
 	y: number;
 }
+enum TemplateType {
+	static = 0,
+	general = 1,
+	text = 2,
+	attr = 3
+}
+interface ITemplatePart {
+	part: string;
+	templateType: TemplateType;
+	forId: string;
+}
+type Template = Array<ITemplatePart>
+
+interface IDictionary<T> {
+	[index: string]: T;
+}
+
+function base64ToArrayBuffer(base64String: string): ArrayBuffer {
+	var byteNumbers = new Array(base64String.length);
+	for (var i = 0; i < base64String.length; i++) {
+		byteNumbers[i] = base64String.charCodeAt(i);
+	}
+	var byteArray = new Uint8Array(byteNumbers);
+	return byteArray.buffer;
+}
 
 class Xmas {
 	static FlakeChars: string = "❄❅❆"
@@ -14,17 +39,20 @@ class Xmas {
 	static Greetings: Array<string> = ["Happy Holidays", "Merry Christmas", "God Jul", "Feliz Navidad", "Wesołych Świąt", "Frohe Weihnachten", "Feliz Natal"];
 	static GreetTimeout: number = 2000;
 
-	css: string = `#thepanel {position:absolute; width:100%; height:100%; z-index:100000; top:0; left:0; background-color:#000; opacity:0.6;}\n
+	css: string = `#thepanel {transform:translateZ(0); position:absolute; overflow:hidden; width:100%; height:100%; z-index:100000; top:0; left:0; background-color:#000; opacity:0.6;}\n
 	.flake {z-index: 1; position:absolute; top:0; left:0; font-size:` + (Xmas.BaseFlakeHeight / 1.3) + `px; width:` + Xmas.BaseFlakeHeight + `px; height:` + Xmas.BaseFlakeHeight + `px; color: #ffffff; opacity: 1.0; text-align: center;}\n
 	.star {z-index: 1; position:absolute; top:0; left:0; font-size:` + (Xmas.BaseStarHeight / 1.3) + `px; width:` + Xmas.BaseStarHeight + `px; height:` + Xmas.BaseStarHeight + `px; color: #ffffff; opacity: 0.6; text-align: center;}\n
 	#thegreeting {font-family: "Comic Sans MS", cursive, sans-serif; z-index: 2; position:absolute; top:0; left:0; right:0; bottom:0; margin:auto; height:300px; width: 100%; font-size:180px; line-height:normal; background-color:transparent; color: #f00000; vertical-align:middle; text-align:center; white-space: nowrap;} 
 	@-webkit-keyframes twinkle { 0% { opacity: 0.6; } 90% { opacity: 0.6; } 95% { opacity: 1.0; }} 
 	@keyframes twinkle { 0% { opacity: 0.6; } 90% { opacity: 0.6; } 95% { opacity: 1.0; }}
 	.star {-webkit-animation: twinkle 5s infinite; animation: twinkle 5s infinite;} 
-	`
-	FlakeTemplate: string = "<div id=\"{$id}\" class=\"flake\">{Xmas.getRandomFlake()}</div>"
-	StarTemplate: string = "<div id=\"{$id}\" class=\"star\">{Xmas.getRandomStar()}</div>"
-	GreetingTemplate: string = "<div id=\"thegreeting\">{this.getMessage()}</div>"
+	`;
+	FlakeTemplateString: string = "<div id=\"{$id}\" class=\"flake\">{:text for $id:Xmas.getRandomFlake()}</div>";
+	StarTemplateString: string = "<div id=\"{$id}\" class=\"star\">{:text for $id:Xmas.getRandomStar()}</div>";
+	GreetingTemplateString: string = "<div id=\"thegreeting\">{:text for thegreeting:this.getMessage()}</div>";
+	FlakeTemplate: Template = null;
+	StarTemplate: Template = null;
+	GreetingTemplate: Template = null;
 	Greeting: HTMLElement = null;
 	Panel: HTMLElement = null;
 	Style: HTMLStyleElement = null;
@@ -46,6 +74,7 @@ class Xmas {
 	greetTime: number = 0;
 	greetPos: number = 0;
 	timeToGreet: boolean = false;
+	mouse: IVec2 = { x: 0, y: 0 };
 
 	static toArray = function(a: ArrayLike<any>) {
 		return Array.prototype.slice.call(a);
@@ -80,40 +109,129 @@ class Xmas {
 	static getRandomStar(): string {
 		return Xmas.getRandomChar(Xmas.StarChars);
 	}
-	static drawTemplate(template: string, parent: HTMLElement, id?: string, context?: any): HTMLElement {
-		var templateParts: Array<string> = [];
+	static compileTemplate(template: string): Array<ITemplatePart> {
+		var templateParts: Array<ITemplatePart> = [];
 		var pos = 0;
-		var s: number = 0, e: number = 0;
+		var s: number = 0;
 		var expr: string = "";
-		var obj: any = {};
+		var next = 1;
+		var templateType: TemplateType = TemplateType.static;
+		var forId: string = null;
 		while (pos < template.length) {
-			s = template.indexOf("{", pos)
-			if (s === -1) {
-				templateParts.push(template.substring(pos));
-				break;
+			switch (template[pos]) {
+				case "\\":
+					pos++;
+					break;
+				case "{":
+					templateParts.push({
+						part: template.substring(s, pos),
+						templateType: templateType,
+						forId: forId
+					});
+					next = pos + 1;
+					if (template.indexOf(":text", next) === next) {
+						//optimize text content replace
+						templateType = TemplateType.text;
+						pos += 5;
+						if (template.indexOf(" for ", pos) === pos + 1) {
+							s = pos += 5;
+							pos = template.indexOf(":", pos);
+							if (pos === -1) {
+								throw "Malfomed template";
+							}
+							forId = template.substring(s, pos).trim();
+						}
+						pos = template.indexOf(":", pos);
+						if (pos === -1) {
+							throw "Malfomed template";
+						}
+					} else if (template.indexOf(":attr", next) === next) {
+						//optimize text content replace
+						templateType = TemplateType.attr;
+						pos += 5;
+						if (template.indexOf(" for ", pos) === pos + 1) {
+							s = pos += 5;
+							pos = template.indexOf(":", pos);
+							if (pos === -1) {
+								throw "Malfomed template";
+							}
+							forId = template.substring(s, pos).trim();
+						} else {
+							pos = template.indexOf(":", pos);
+							if (pos === -1) {
+								throw "Malfomed template";
+							}
+						}
+					}
+					s = pos + 1;
+					break;
+				case "}":
+					if (pos > 1) {
+						templateParts.push({
+							part: template.substring(s, pos),
+							templateType: templateType,
+							forId: forId
+						});
+						s = pos + 1;
+						templateType = TemplateType.static;
+						forId = null;
+					}
+					break;
 			}
-			templateParts.push(template.substring(pos, s));
-			s++;
-			e = template.indexOf("}", s);
-			if (e === -1) {
-				throw ("DUDE! your template sucks");
-			}
-			expr = "return " + template.substring(s, e);
-			if (id !== undefined && expr.indexOf("$id") !== -1) {
-				expr = expr.replace("$id", "\"id\"");
-			}
-			var fn = new Function(expr);
-			templateParts.push(fn.call(context || this));
-			pos = ++e;
+			pos++;
 		}
-		template = templateParts.join("");
+		if (s !== pos) {
+			templateParts.push({
+				part: template.substring(s, pos),
+				templateType: templateType,
+				forId: forId
+			});
+		}
+		return templateParts;
+	}
+	static drawTemplate(template: Template, parent: HTMLElement, id?: string, context?: any): HTMLElement {
+		var templateParts: Array<string> = [];
+		var p = 0;
+		var obj: any = {};
+		var html = ""
+		var el: HTMLElement = null;
+		var existing: HTMLElement = id ? (el = <HTMLElement>parent.querySelector("#" + id)) : null;
+		var expr = "";
+		var fn: Function = null;
+		for (p = 0; p < template.length; p++) {
+			switch (template[p].templateType) {
+				case TemplateType.text:
+					expr = template[p].part.replace("$id", id);
+					fn = new Function("return " + expr);
+					expr = fn.call(context || this);
+					if (existing) {
+						parent.querySelector("#" + template[p].forId).textContent = expr;
+					} else {
+						templateParts.push(expr);
+					}
+					break;
+				case TemplateType.attr:
+					break;
+				case TemplateType.general:
+					break;
+				default:
+					//static
+					if (!existing) {
+						expr = template[p].part.replace("$id", id);
+						templateParts.push(expr);
+					}
+					break;
+			}
+		}
+		if (!existing) {
+			html = templateParts.join("");
 
-		var el = Xmas.createElement(template);
-		var existing: HTMLElement;
-		if (existing = <HTMLElement>parent.querySelector("#" + id)) {
-			parent.replaceChild(el, existing);
-		} else {
-			parent.appendChild(el);
+			el = Xmas.createElement(html);
+			if (existing) {
+				parent.replaceChild(el, existing);
+			} else {
+				parent.appendChild(el);
+			}
 		}
 		return el;
 	}
@@ -126,11 +244,18 @@ class Xmas {
 	getMessage(): string {
 		return Xmas.Greetings[this.greetPos];
 	}
+	mousemove(event: MouseEvent): void {
+		this.mouse.x = event.clientX;
+		this.mouse.y = event.clientY;
+	}
 	start() {
 		var i: number;
 		var windowWidth = Xmas.WindowWidth = window.innerWidth;
 		var windowHeight = Xmas.WindowHeight = window.innerHeight;
 		var starSpan = windowHeight / 3.0;
+		this.StarTemplate = Xmas.compileTemplate(this.StarTemplateString);
+		this.FlakeTemplate = Xmas.compileTemplate(this.FlakeTemplateString);
+		this.GreetingTemplate = Xmas.compileTemplate(this.GreetingTemplateString);
 		this.Greeting = Xmas.drawTemplate(this.GreetingTemplate, this.Panel, "thegreeting", this);
 		for (i = 0; i < this.starCount; i++) {
 			this.stars.push(Xmas.drawTemplate(this.StarTemplate, this.Panel, "star" + i));
@@ -148,11 +273,18 @@ class Xmas {
 			this.flakes[i].style.transform = "translate(" + this.flakePos[i].x + "px, " + this.flakePos[i].y + "px) scale(" + this.flakeSpeed[i] + ") rotateY(" + this.flakeRotation[i] + "deg)";
 			this.flakes[i].style.zIndex = (this.flakeScale[i] < 1.0 ? 1 : 3).toString();
 		}
-		this.Panel.addEventListener("click", () => { this.destroy(); });
-		this.Panel.addEventListener("touchend", () => { this.destroy(); });
-		this.Panel.addEventListener("pointerup", () => { this.destroy(); });
+		this.Panel.addEventListener("click", this.destroy.bind(this));
+		this.Panel.addEventListener("touchend", this.destroy.bind(this));
+		this.Panel.addEventListener("pointerup", this.destroy.bind(this));
+		this.Panel.addEventListener("mousemove", this.mousemove.bind(this));
 		document.body.appendChild(this.Style);
 		document.body.appendChild(this.Panel);
+		/*
+		AudioManager.loadSound("holiday", base64Music, true).then((id:string) => {
+			AudioManager.playSound(id);
+		});
+		*/
+
 		this.msPerFrame = 1000.0 / Xmas.FPS;
 		this.running = true;
 		this.oldTime = window.performance.now();
@@ -165,14 +297,18 @@ class Xmas {
 		this.time = window.performance.now();
 		var i: number;
 		var greetDiff = this.time - this.greetTime;
+		var flakeHit = false;
 		if (greetDiff > Xmas.GreetTimeout) {
 			this.timeToGreet = true;
 			this.greetTime = this.time;
 			this.greetPos = (this.greetPos + 1) % Xmas.Greetings.length;
 		}
 		for (i = 0; i < this.flakeCount; i++) {
-			if (this.flakePos[i].y > Xmas.WindowHeight) {
+			flakeHit = ((this.mouse.x >= this.flakePos[i].x) && (this.mouse.x - this.flakePos[i].x) < Xmas.BaseFlakeHeight * this.flakeScale[i]) &&
+				((this.mouse.y >= this.flakePos[i].y) && (this.mouse.y - this.flakePos[i].y) < Xmas.BaseFlakeHeight * this.flakeScale[i]);
+			if (flakeHit || this.flakePos[i].y > Xmas.WindowHeight) {
 				this.flakePos[i].y -= (Xmas.WindowHeight + 100);
+				this.flakePos[i].x = Xmas.randomInt(Xmas.WindowWidth);
 			}
 			this.flakePos[i].y += Xmas.WindowHeight * this.flakeSpeed[i] / Xmas.FPS;
 			this.flakeRotation[i] = (this.flakeRotation[i] + this.flakeRotationSpeed[i]) % 360;
@@ -183,6 +319,7 @@ class Xmas {
 		var i: number;
 		if (this.timeToGreet) {
 			this.Greeting = Xmas.drawTemplate(this.GreetingTemplate, this.Panel, "thegreeting", this);
+			this.timeToGreet = false;
 		}
 		for (i = 0; i < this.flakeCount; i++) {
 			this.flakes[i].style.transform = "translate(" + this.flakePos[i].x + "px, " + this.flakePos[i].y + "px) scale(" + this.flakeScale[i] + ") rotateY(" + this.flakeRotation[i] + "deg)";
@@ -213,4 +350,5 @@ class Xmas {
 		this.flakeScale.length = 0;
 	}
 }
+
 var xmas = new Xmas();
